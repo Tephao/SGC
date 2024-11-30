acb52de3e4fdefaa15e30b7ed6cf8e1fce58deac
Stepherson Borges dos Santos <stepherson.bs@gmail.com>
2024-11-29 21:51:02 -0300

    Criado arquivo js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function abrirdadospessoais() {
+    window.open("dadospessoais.html", '_self');
+}
+
+function abrirformacao() {
+    window.open("formacao.html", '_self');
+}
+
+function abrirexperiencias() {
+    window.open("experiencias.html", '_self');
+}
+
+function abrirlogin() {
+    window.open("login.html", '_self');
+}
+
+function abrirmenu() {
+    window.open("menu.html", '_self');
+}
+
+function acessibilidade(){
+    document.getElementById("login").style.backgroundColor='red';
+    document.getElementById("div-login").style.width='44%';
+    document.getElementById("div-login").style.backgroundColor='yellow';
+    document.getElementById("label-usuario").style.fontSize="30px";
+    document.getElementById("label-senha").style.fontSize="30px";
+    document.getElementById("input-usuario").style.fontSize="30px";
+    document.getElementById("input-login").style.fontSize="30px";
+}
